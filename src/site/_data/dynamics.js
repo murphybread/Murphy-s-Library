18ed5b2d30d0e6b91e502d5de4f8f0b105c21127
murphybread <nargene@gmail.com>
2024-01-02 23:40:30 +0900

    Update dynamics.js test path modify and custom is at the end

--- src/site/_data/dynamics.js
+++ src/site/_data/dynamics.js
@@ -28,18 +28,38 @@ const generateComponentPaths = async (namespace, slots) => {
   return data;
 };
 
+// const generateStylesPaths = async () => {
+//   try {
+//     const tree = await fsFileTree(`${STYLE_PATH}`);
+//     let comps = Object.keys(tree).map((p) =>
+//       `/styles/user/${p}`.replace(".scss", ".css")
+//     );
+//     comps.sort();
+//     return comps;
+//   } catch {
+//     return [];
+//   }
+// };
+
 const generateStylesPaths = async () => {
   try {
-    const tree = await fsFileTree(`${STYLE_PATH}`);
-    let comps = Object.keys(tree).map((p) =>
-      `/styles/user/${p}`.replace(".scss", ".css")
-    );
+    // Adjusted to the correct styles directory
+    const tree = await fsFileTree(`src/site/styles/`);
+    let comps = Object.keys(tree)
+      .map((p) => `/styles/${p}`.replace('.scss', '.css'))
+      // Remove custom-style.css if it's already in the array to re-add it later
+      .filter((path) => !path.endsWith('custom-style.css'));
+    // Sort the array to maintain a consistent order
     comps.sort();
+    // Add custom-style.css at the end to ensure it's the last stylesheet
+    comps.push('/styles/custom-style.css');
     return comps;
   } catch {
     return [];
   }
 };
+
+
 
 module.exports = async () => {
   const data = {};
