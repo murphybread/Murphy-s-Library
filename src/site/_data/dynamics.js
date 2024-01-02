667e5dbd8cdbc35a6ff566a15e72a69fac06aadb
murphybread <nargene@gmail.com>
2024-01-02 23:59:21 +0900

    Update dynamics.js final style change

--- src/site/_data/dynamics.js
+++ src/site/_data/dynamics.js
@@ -1,7 +1,7 @@
 const fsFileTree = require("fs-file-tree");
 
 const BASE_PATH = "src/site/_includes/components/user";
-const STYLE_PATH = "src/site/styles/user";
+const STYLE_PATH = "src/site/styles/";
 const NAMESPACES = ["index", "notes", "common"];
 const SLOTS = ["head", "header", "beforeContent", "afterContent", "footer"];
 const FILE_TREE_NAMESPACE = "filetree";
@@ -45,23 +45,7 @@ const generateStylesPaths = async () => {
   }
 };
 
-// const generateStylesPaths = async () => {
-//   try {
-//     // Adjusted to the correct styles directory
-//     const tree = await fsFileTree(`src/site/styles/`);
-//     let comps = Object.keys(tree)
-//       .map((p) => `/styles/${p}`.replace('.scss', '.css'))
-//       // Remove custom-style.css if it's already in the array to re-add it later
-//       .filter((path) => !path.endsWith('custom-style.css'));
-//     // Sort the array to maintain a consistent order
-//     comps.sort();
-//     // Add custom-style.css at the end to ensure it's the last stylesheet
-//     comps.push('/styles/custom-style.css');
-//     return comps;
-//   } catch {
-//     return [];
-//   }
-// };
+
 
 
 
